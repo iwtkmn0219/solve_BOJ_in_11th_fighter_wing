244bae24ee517692e40bb25ff7146fb3ad1d74ae
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-07-27 06:34:07 +0000

    250727 16112.cpp

---
+++ 16112.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPR(i, a, b) for (int i = a; i >= b; i--)
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; }
+#define SORTV(v) sort(v.begin(), v.end())
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; }
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int n, m;
+vi v;
+vector<ll> p_sum;
+
+int main() {
+	FASTIO;
+	cin >> n >> m;
+	VINPUT(v, n);
+	sort(v.begin(), v.end());
+	REP(i, 0, v.size()) {
+		if (i == 0) p_sum.PB(v[0]);
+		else {
+			p_sum.PB(v[i] + p_sum[i - 1]);
+		}
+	}
+	ll sum = 0;
+	REP(i, 0, v.size()) {
+		sum += p_sum[min(i + m, int(v.size()) - 1)] - p_sum[i];
+		NWATCH(sum);
+	}
+	cout << sum;
+}
