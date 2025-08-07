7e719352fef1400f587f70f4f41af6e42f8ea522
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-08-07 12:24:51 +0000

    250807 marathon

---
+++ solved/30794.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPR(i, a, b) for (int i = a; i >= b; i--)
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; }
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; }
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int main() {
+	FASTIO;
+	int level;
+	string command;
+	cin >> level >> command;
+	if (command == "miss") {
+		cout << 0;
+	} else if (command == "bad") {
+		cout << level * 200;
+	} else if (command == "cool") {
+		cout << level * 400;
+	} else if (command == "great") {
+		cout << level * 600;
+	} else if (command == "perfect") {
+		cout << level * 1000;
+	}
+}
---
+++ solved/9063.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPR(i, a, b) for (int i = a; i >= b; i--)
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; }
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; }
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int n;
+int x = 10001, xx = -10001, y = 10001, yy = -10001;
+
+int main() {
+	FASTIO;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp1, tmp2; cin >> tmp1 >> tmp2;
+		x = min(x, tmp1);
+		xx = max(xx, tmp1);
+		y = min(y, tmp2);
+		yy = max(yy, tmp2);
+	}
+	cout << (xx - x) * (yy - y);
+}
