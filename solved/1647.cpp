7681a05169f8d9c2910338ca9432d114eb11adae
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-09-01 11:13:28 +0000

    2025-09-01(#215 > #213) MST
    1647: MST
    MST 복습 겸 골라서 푼 문제. 조금 신박(?)한 문제였음. MST를 생성하고 최대 간선을 끊어내는 문제.

---
+++ solved/1647.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct Edge {
+	int cost, u, v;
+	bool operator<(const Edge &e) {
+		if (cost != e.cost) return cost < e.cost;
+		else if (u != e.u) return u < e.u;
+		return v < e.v;
+	}
+};
+
+int n, m;
+vi parrent;
+vector<Edge> ve;
+
+int find(int x) {
+	if (parrent[x] == x) return x;
+	return parrent[x] = find(parrent[x]);
+}
+void uni(int x, int y) {
+	parrent[find(y)] = find(x);
+}
+bool same_root(int x, int y) {
+	return find(x) == find(y);
+}
+
+int main() {
+	FASTIO;
+	cin >> n >> m;
+	parrent.resize(n + 1, 0);
+	for (int i = 0; i < m; i++) {
+		int u, v, w; cin >> u >> v >> w;
+		ve.PB({w, u, v});
+	}
+	SORT(ve);
+	for (int i = 1; i <= n; i++) parrent[i] = i;
+	
+	int result = 0;
+	int max_cost = 0;
+	for (auto e: ve) {
+		if (!same_root(e.u, e.v)) {
+			uni(e.u, e.v);
+			max_cost = max(max_cost, e.cost);
+			result += e.cost;
+		}
+	}
+	cout << result - max_cost;
+}
