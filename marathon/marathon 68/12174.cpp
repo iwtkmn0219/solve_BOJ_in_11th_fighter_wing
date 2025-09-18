e551744dc431aaeafb0467bc62e73645f45dd02e
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-09-18 12:15:45 +0000

    2025-09-18(#178 > #178[-]) random marathon 68 (8/8)

---
+++ marathon/marathon 68/12174.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int tc;
+
+int main() {
+	FASTIO;
+	cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		int byte; string s;
+		cin >> byte >> s;
+		int tmp = 0;
+		cout << "Case #" << i + 1 << ": ";
+		for (int i = 0; i < byte * 8; i += 8) {
+			for (int j = 7; j >= 0; j--) {
+				if (s[i + j] == 'I') {
+					tmp |= (1 << (7 - j));
+				}
+			}
+			cout << (char)tmp;
+			tmp = 0;
+		}
+		cout << endl;
+	}
+}
