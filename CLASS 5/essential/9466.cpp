b857e296a4282dab6b2d071fa385eb587f2f28f6
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-09-20 12:08:37 +0000

    2025-09-20(#178 > #159[+19]) CLASS 5 Essential
    9466: DFS
    DFS입니다. 다만, 단순한 DFS가 아닌 방문을 두 번 체크하는 문제입니다. 시간복잡도를 O(n)으로 만드는 것이 목표인 문제입니다.

---
+++ CLASS 5/essential/9466.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int tc;
+int chk = 0;
+
+int main() {
+	FASTIO;
+	cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		vi v, result(n + 1, 0);
+		vi visited(n + 1, 0);
+		v.PB(0);
+		VINPUT(v, n);
+		for (int i = 1; i <= n; i++) {
+			if (visited[i]) continue;
+			NWATCH(i);
+			int curr = i;
+			while (true) {
+				visited[curr] = 1;
+				int next = v[curr];
+				if (!visited[next]) {
+					curr = next;
+				} else if (!result[next]) {
+					result[curr] = 1;
+					for (int j = next; j != curr; j = v[j]) {
+						result[j] = 1;
+					}
+				} else {
+					break;
+				}
+			}
+			for (int j = i; result[j] == 0; j = v[j]) {
+				NWATCH(j);
+				result[j] = 2;
+			}
+			NVOUTPUT(visited);
+			NVOUTPUT(result);
+		}
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (result[i] != 1) cnt++;
+		}
+		cout << cnt << endl;
+	}
+}
