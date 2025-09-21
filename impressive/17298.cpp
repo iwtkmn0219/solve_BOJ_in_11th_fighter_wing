14a281fe690af7368fdb88898265929e2a22844e
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-09-21 09:00:23 +0000

    2025-09-21(#154 > #154[-]) GOLD (impressive)
    17298: 스택
    스택을 전혀 떠올리지 못했던 문제이자, 알고리즘 분류를 보고도 풀지 못했던 문제입니다. log시간대에 풀어야한다는 집착이 어느정도 작용했던 것도 있지만, 스택, 큐를 어떤식으로 활용해야하는지 잘 생각해야겠다고 느꼈습니다. 충분히 다시 풀어볼 만한 문제라고 생각하여 impressive에 저장합니다.

---
+++ impressive/17298.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int n;
+vi v, result;
+stack<int> s;
+
+int main() {
+	FASTIO;
+	cin >> n;
+	result.resize(n, 0);
+	VINPUT(v, n);
+	s.push(0);
+	for (int i = 1; i < n; i++) {
+		while (!s.empty() && v[s.top()] < v[i]) {
+			result[s.top()] = v[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+	while (!s.empty()) {
+		result[s.top()] = -1;
+		s.pop();
+	}
+	VOUTPUT(result);
+}
