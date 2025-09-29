4366c739cf74029c5e3f6fb80ad38960f825b650
iwtkmn0219 <83506704+iwtkmn0219@users.noreply.github.com>
2025-09-29 11:28:27 +0000

    2025-09-29(#135 > #133[+2]) random marathon 69

---
+++ marathon/marathon 69/16466.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+int n;
+vi v;
+
+int main() {
+	FASTIO;
+	cin >> n;
+	VINPUT(v, n);
+	SORT(v);
+	int curr = 1;
+	for (int i = 0; i < n; i++) {
+		if (v[i] != curr) {
+			break;
+		} else {
+			curr++;
+		}
+	}
+	cout << curr;
+}
---
+++ marathon/marathon 69/2153.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define INF 1e9
+#define dir4 {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+#define dir8 {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+#define board_condition(row, col, n, m) (row >= 0 && row < n && col >= 0 && col < m)
+#define FASTIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define F first
+#define S second
+#define PB push_back
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define REV(x) reverse(x.begin(), x.end())
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define VINPUT(v, n) REP(i, 0, n) { int tmp; cin >> tmp; v.PB(tmp); }
+#define VOUTPUT(v) REP(i, 0, v.size()) { cout << v[i] << " "; } cout << endl
+#define V2DINPUT(v, n, m) REP(i, 0, n) { vi tmpv; REP(j, 0, m) { int tmp; cin >> tmp; tmpv.PB(tmp); } v.PB(tmpv); }
+#define V2DOUTPUT(v) REP(i, 0, v.size()) { REP(j, 0, v[i].size()) { cout << v[i][j] << " "; } cout << endl; } cout << endl
+#define WATCH(x) cout << #x << " : " << x << endl
+#define DEBUG(x) cout << "DB - " << x << endl
+#define PRINT(x) cout <<  x << endl
+#define NVOUTPUT(x)
+#define NV2DOUTPUT(x)
+#define NWATCH(x)
+#define NDEBUG(x)
+#define NPRINT(x)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+struct point {
+	int x, y;
+};
+
+string s;
+vi prime_number(1501, 1);
+
+void init_prime_number() {
+	prime_number[0] = 0;
+	for (int i = 2; i * i <= 1500; i++) {
+		if (!prime_number[i]) continue;
+		for (int j = i + i; j <= 1500; j += i) {
+			prime_number[j] = 0;
+		}
+	}
+}
+
+int main() {
+	FASTIO;
+	init_prime_number();
+	cin >> s;
+	int num = 0;
+	for (auto word: s) {
+		if (word < 'a') {
+			num += word - 'A' + 27;
+		} else {
+			num += word - 'a' + 1;
+		}
+	}
+	NWATCH(num);
+	if (prime_number[num]) {
+		cout << "It is a prime word.";
+	} else {
+		cout << "It is not a prime word.";
+	}
+}
